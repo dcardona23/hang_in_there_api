816655a1e50b5a003162eb47a8c29ebe44b2665e
Danielle Cardona <danielle.cardona23@gmail.com>
2024-10-22 14:34:04 -0600

    feat: add show function and tests

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -1,3 +1,17 @@
-class Api::v1::PostersController < ApplicationController
+class Api::V1::PostersController < ApplicationController
     
+
+
+
+
+
+
+
+
+
+
+    def show
+        render json: Poster.find(params[:id])
+    end
+
 end
--- spec/requests/api/v1/posters_request_spec.rb
+++ spec/requests/api/v1/posters_request_spec.rb
@@ -48,7 +48,36 @@ require 'rails_helper'
 
 
 
+describe "Posters API" do
+    it "can get one poster by its id" do
+        id = Poster.create(name: "MEDIOCRITY", description: "Dreams are just that—dreams.", price: 127.00, year: 2021, vintage: false, img_url: "https://images.unsplash.com/photo-1551993005-75c4131b6bd8").id
+    
+        get "/api/v1/posters/#{id}"
+    
+        poster = JSON.parse(response.body, symbolize_names: true)
+    
+        expect(response).to be_successful
 
+            expect(poster).to have_key(:id)
+            expect(poster[:id]).to be_an(Integer)
+    
+            expect(poster).to have_key(:name)
+            expect(poster[:name]).to be_a(String)
+    
+            expect(poster).to have_key(:description)
+            expect(poster[:description]).to be_a(String)
+    
+            expect(poster).to have_key(:price)
+            expect(poster[:price]).to be_a(Float)
+
+            expect(poster).to have_key(:vintage)
+            expect(poster[:vintage]).to be_in([true, false])
+
+            expect(poster).to have_key(:img_url)
+            expect(poster[:img_url]).to be_a(String)
+
+    end
+end
 
 
 
